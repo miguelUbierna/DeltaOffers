d01dc5d626ef0d34880cd1d5e890c865959aadbf
miguelUbierna <m.ubiernagutierrez@gmail.com>
2024-03-14 19:36:35 +0100

    Pasado a POO en la UBU y otras modificaciones

--- Scrapers/ScrapingUBU/scrapingUBU.py
+++ Scrapers/ScrapingUBU/scrapingUBU.py
@@ -1,129 +1,239 @@
 import requests
 from bs4 import BeautifulSoup
 
-titulos = []
-descripciones = []
-list_fechas_convocatorias = []
-list_inicio = []
-list_fin = []
-convocantes = []
-destinatarios = []
-estados = []
-enlaces = []
-categorias = []
 
-ubu_general = []
+class UBUScraper:
+    def __init__(self):
+        self.titulos = []
+        self.estados = []
+        self.descripciones = []
+        self.list_fechas_convocatorias = []
+        self.list_inicio = []
+        self.list_fin = []
+        self.convocantes = []
+        self.destinatarios = []
+        self.categorias = []
+        self.enlaces = []
+        self.universidad = []
+        self.ubu_general = []
+        self.contador_cerradas = 0
 
+    def obtener_titulos(self, item):
+        titulo = item.find(
+            'div', class_='field-content').find('a').get_text(strip=True)
+        self.titulos.append(titulo)
 
-# Link para extraer los datos.
-link_ubu = 'https://www.ubu.es/trabaja-en-la-ubu'
-
-# Para enviar una solicitud a la página y como resultado nos da una respuesta-
-response = requests.get(link_ubu)
-
-# Comprobamos si la solicitud fue exitosa.
-if response.status_code == 200:
-    # El response.text es para obtener el texto de esa respuesta/resultado que me envió la pagina.
-    # La variable soup es importante porque es la que nos va a permitir obtener elementos en una página web.
-    # El parser es lxml para obtener el código HTML de la respuesta obtenida.
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    # Para buscar un elemento por su etiqueta.
-    etiqueta_pagina = soup.find_all(
-        'div', class_='views-field views-field-field---last-application-date')
-
-    for item in etiqueta_pagina:
-        titulo = item.find('div', class_='field-content').get_text(strip=True)
-        titulos.append(titulo)
+    def obtener_estados(self, item):
         estado = item.find('div').get_text(strip=True)
-        estados.append(estado)
+        # En el momento en el que estoy haciendo el scraping, no hay ninguna convocatoria en resolución.
+        # Como no se, cual será el contenido de este tipo de etiquetas, si el estado de la convocatoria,
+        # No es ni en plazo ni cerrado, automaticamente podré como estado: "EN RESOLUCIÓN"
+        if estado != 'EN PLAZO' and estado != 'CONVOCATORIA CERRADA':
+            self.estados.append('EN RESOLUCIÓN')
+        else:
+            self.estados.append(estado)
 
+    def obtener_enlaces_subpagina(self, item):
         # Proceso de obtención de subpágina.
-        enlace_subpagina = 'https://www.ubu.es'+item.find('a')['href']
-        enlaces.append(enlace_subpagina)
+        enlace_subpagina = 'https://www.ubu.es' + item.find('a')['href']
+        self.enlaces.append(enlace_subpagina)
         respuesta_subpagina = requests.get(enlace_subpagina)
+        return respuesta_subpagina
 
-        if respuesta_subpagina.status_code == 200:
-            sub_soup = BeautifulSoup(respuesta_subpagina.text, 'lxml')
-            contenedores_subpagina = sub_soup.find_all('article')
+    def obtener_descripciones(self, contenedor):
+        descripcion = contenedor.find('div', class_='field-item even', property='content:encoded').p.get_text(
+            strip=True)
+        self.descripciones.append(descripcion)
 
-            for contenedor in contenedores_subpagina:
-                descripcion = contenedor.find(
-                    'div', class_='field-item even', property='content:encoded').p.get_text(strip=True)
-                texto_limpio_descripcion = descripcion.replace('\xa0', ' ')
-                descripciones.append(texto_limpio_descripcion)
+    def obtener_fechas_convocatorias(self, contenedor):
+        fecha_convocatoria = contenedor.find(
+            'span', class_='date-display-single')['content']
+        self.list_fechas_convocatorias.append(fecha_convocatoria)
 
-                fecha_convocatoria = contenedor.find(
-                    'span', class_='date-display-single')['content']
-                list_fechas_convocatorias.append(fecha_convocatoria)
+    def obtener_plazos_solicitud(self, contenedor):
+        if str(contenedor.find('div', class_='field field-name-field-presentation-date field-type-datetime '
+                               'field-label-above').find('span')['class']) != "['date-display-single']":
 
-                if str(contenedor.find('div', class_='field field-name-field-presentation-date field-type-datetime field-label-above').find('span')['class']) != "['date-display-single']":
+            fecha_inicio_solicitud = contenedor.find(
+                'span', class_='date-display-start')['content']
+            self.list_inicio.append(fecha_inicio_solicitud)
 
-                    fecha_inicio_solicitud = contenedor.find(
-                        'span', class_='date-display-start')['content']
-                    list_inicio.append(fecha_inicio_solicitud)
+            fecha_fin_solicitud = contenedor.find(
+                'span', class_='date-display-end')['content']
+            self.list_fin.append(fecha_fin_solicitud)
 
-                    fecha_fin_solicitud = contenedor.find(
-                        'span', class_='date-display-end')['content']
-                    list_fin.append(fecha_fin_solicitud)
+        else:
+            self.list_inicio.append('No Especificada')
+            fecha_fin_solicitud = contenedor.find('div', class_='field field-name-field-presentation-date '
+                                                  'field-type-datetime field-label-above').find(
+                'span', class_='date-display-single')['content']
+            self.list_fin.append(fecha_fin_solicitud)
+
+    def obtener_convocantes(self, contenedor, convocante):
+        if contenedor.find('div', class_='field field-name-field-convener field-type-text-long field-label-above') == None:
+            self.convocantes.append('No especificado')
+        else:
+            contenedor_convocante = contenedor.find(
+                'div', class_='field field-name-field-convener field-type-text-long field-label-above').find_all('p')
+            for item in contenedor_convocante:
+                convocante += item.get_text(strip=True) + ' '
+            self.convocantes.append(convocante)
+
+    def obtener_destinatarios(self, contenedor):
+        if contenedor.find('div', class_='field field-name-field-receiver field-type-text-long field-label-above') \
+                is not None:
+            contenedor_destinatarios = contenedor.find(
+                'div', class_='field field-name-field-receiver field-type-text-long field-label-above').find(
+                'div', class_='field-item even').get_text(strip=True)
+            self.destinatarios.append(contenedor_destinatarios)
+        else:
+            self.destinatarios.append('No se han encontrado destinatarios')
+
+    def obtener_categorias(self, contenedor):
+        categoria = contenedor.find(
+            'div', class_='field-item odd').a.get_text(strip=True)
+
+        if categoria == 'Convocatorias personal docente' or categoria == 'Investigación: adscritas a proyectos':
+            self.categorias.append('PDI')
+        elif categoria == 'Convocatorias PAS':
+            self.categorias.append('PAS')
+        else:
+            self.categorias.append('No especificada')
+
+    def obtener_universidad(self):
+        for _ in range(len(self.titulos)):
+            self.universidad.append('Universidad de Burgos')
+
+    def obtener_datos(self, link):
+        convocante = ''
+        # Para enviar una solicitud a la página y como resultado nos da una respuesta
+        response = requests.get(link)
+
+        # Comprobamos si la solicitud fue exitosa.
+        if response.status_code == 200:
+            # El response.text es para obtener el texto de esa respuesta/resultado que me envió la pagina.
+            # La variable soup es importante porque es la que nos va a permitir obtener elementos en una página web.
+            # El parser es lxml para obtener el código HTML de la respuesta obtenida.
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            # Para buscar un elemento por su etiqueta.
+            etiqueta_pagina = soup.find_all(
+                'div', class_='views-field views-field-field---last-application-date')
+
+            for item in etiqueta_pagina:
+                self.obtener_titulos(item)
+                self.obtener_estados(item)
+                respuesta_subpagina = self.obtener_enlaces_subpagina(item)
+
+                if respuesta_subpagina.status_code == 200:
+                    sub_soup = BeautifulSoup(respuesta_subpagina.text, 'lxml')
+                    contenedores_subpagina = sub_soup.find_all('article')
+
+                    for contenedor in contenedores_subpagina:
+                        self.obtener_descripciones(contenedor)
+                        self.obtener_fechas_convocatorias(contenedor)
+                        self.obtener_plazos_solicitud(contenedor)
+                        self.obtener_convocantes(contenedor, convocante)
+                        self.obtener_destinatarios(contenedor)
+                        self.obtener_categorias(contenedor)
 
                 else:
-                    list_inicio.append('No Especificada')
-                    fecha_fin_solicitud = contenedor.find(
-                        'div', class_='field field-name-field-presentation-date field-type-datetime field-label-above').find('span', class_='date-display-single')['content']
-                    list_fin.append(fecha_fin_solicitud)
-
-                if contenedor.find('div', class_='field field-name-field-receiver field-type-text-long field-label-above') != None:
-                    contenedor_destinatarios = contenedor.find(
-                        'div', class_='field field-name-field-receiver field-type-text-long field-label-above').find('div', class_='field-item even').get_text(strip=True)
-                    destinatarios.append(contenedor_destinatarios)
-                else:
-                    destinatarios.append('No se han encontrado destinatarios')
-
-                categoria = contenedor.find(
-                    'div', class_='field-item odd').a.get_text(strip=True)
-
-                if categoria == 'Convocatorias personal docente' or categoria == 'Investigación: adscritas a proyectos':
-                    categorias.append('PDI')
-                elif categoria == 'Convocatorias PAS':
-                    categorias.append('PAS')
-                else:
-                    categorias.append('No especificada')
+                    print(f'La solicitud ha fallado y su código de estado es el {
+                        respuesta_subpagina.status_code}')
 
         else:
             print(f'La solicitud ha fallado y su código de estado es el {
-                  respuesta_subpagina.status_code}')
+                response.status_code}')
 
-else:
-    print(f'La solicitud ha fallado y su código de estado es el {
-          response.status_code}')
+    def añadir_a_estructura_general(self):
+        self.ubu_general.append(self.titulos)
+        self.ubu_general.append(self.estados)
+        self.ubu_general.append(self.descripciones)
+        self.ubu_general.append(self.list_fechas_convocatorias)
+        self.ubu_general.append(self.list_inicio)
+        self.ubu_general.append(self.list_fin)
+        self.ubu_general.append(self.convocantes)
+        self.ubu_general.append(self.destinatarios)
+        self.ubu_general.append(self.categorias)
+        self.ubu_general.append(self.enlaces)
+        self.ubu_general.append(self.universidad)
+
+        return self.ubu_general
+
+    def tabla_limpia(self):
+        for fila in self.ubu_general:
+            for i, item in enumerate(fila):
+                fila[i] = item.replace('\xa0', ' ')
+
+    def contar_convocatorias_cerradas(self):
+        for item in self.estados:
+            if item == 'CONVOCATORIA CERRADA':
+                self.contador_cerradas += 1
+            else:
+                # Si encuentro una abierta, o en proceso, vuelvo a empezar a contar
+                self.contador_cerradas = 0
+        # Si he recorrido 30 convocatorias y no he encontrado ninguna abierta, suponemos que ya no quedarán mas abiertas.
+        if self.contador_cerradas >= 30:
+            return True
+        else:
+            return False
+
+    def extraer_datos_paginas(self):
+        contador_paginas = 0
+        # Link para extraer los datos.
+        link_ubu = 'https://www.ubu.es/trabaja-en-la-ubu'
+        while not self.contar_convocatorias_cerradas():
+            if contador_paginas == 0:
+                self.obtener_datos(link_ubu)
+            else:
+                link_ubu_siguientes_paginas = 'https://www.ubu.es/trabaja-en-la-ubu?field_term_interest_tid=109&page=' + \
+                    str(contador_paginas)
+                self.obtener_datos(link_ubu_siguientes_paginas)
+                link_ubu_siguientes_paginas = ''
+            contador_paginas += 1
 
 
-print(len(titulos))
-print(len(descripciones))
-print(len(list_fechas_convocatorias))
-print(len(list_inicio))
-print(len(list_fin))
-print(len(destinatarios))
-print(len(estados))
-print(len(enlaces))
-print(len(categorias))
+scraper = UBUScraper()
+scraper.extraer_datos_paginas()
+scraper.obtener_universidad()
+scraper.añadir_a_estructura_general()
+scraper.tabla_limpia()
 
 
-ubu_general.append(titulos)
-ubu_general.append(descripciones)
-ubu_general.append(list_fechas_convocatorias)
-ubu_general.append(list_inicio)
-ubu_general.append(list_fin)
-ubu_general.append(destinatarios)
-ubu_general.append(estados)
-ubu_general.append(enlaces)
-ubu_general.append(categorias)
+print(scraper.titulos)
+print(scraper.estados)
+print(scraper.descripciones)
+print(scraper.list_fechas_convocatorias)
+print(scraper.list_inicio)
+print(scraper.list_fin)
+print(scraper.convocantes)
+print(scraper.destinatarios)
+print(scraper.categorias)
+print(scraper.enlaces)
+print(scraper.universidad)
 
-for i in range(len(titulos)):
-    convocantes.append('Universidad de Burgos')
 
-ubu_general.append(convocantes)
-print('***************************************************************')
+print(len(scraper.titulos))
+print(len(scraper.estados))
+print(len(scraper.descripciones))
+print(len(scraper.list_fechas_convocatorias))
+print(len(scraper.list_inicio))
+print(len(scraper.list_fin))
+print(len(scraper.convocantes))
+print(len(scraper.destinatarios))
+print(len(scraper.categorias))
+print(len(scraper.enlaces))
+print(len(scraper.universidad))
 
-print(ubu_general)
+
+print(scraper.titulos[11])
+print(scraper.estados[11])
+print(scraper.descripciones[11])
+print(scraper.list_fechas_convocatorias[11])
+print(scraper.list_inicio[11])
+print(scraper.list_fin[11])
+print(scraper.convocantes[11])
+print(scraper.destinatarios[11])
+print(scraper.categorias[11])
+print(scraper.enlaces[11])
+print(scraper.universidad[11])
--- Scrapers/ScrapingULE/scrapingULE.py
+++ Scrapers/ScrapingULE/scrapingULE.py
@@ -2,16 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-
-titulos = []
 list_fechas_convocatorias = []
+titulos = []
+enlaces = []
+categorias = []
+tipos = []
 list_inicio = []
 list_fin = []
 convocantes = []
 nombre_plazas = []
+convocatorias_asociadas = []
 estados = []
-enlaces = []
-categorias = []
+descripciones = []
+documentos_adjuntos = []
+
 
 ule_general = []
 
@@ -27,91 +31,169 @@ def determinar_plazo(fecha_fin):
         estados.append('CONVOCATORIA CERRADA')
 
 
-link_ule = 'https://www.unileon.es/actualidad/convocatorias'
+def obtener_datos(link_ule):
+    response = requests.get(link_ule)
 
-response = requests.get(link_ule)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'lxml')
+        etiqueta_pagina = soup.find_all(
+            'td', class_='views-field views-field-created views-field-title')
 
-    etiqueta_pagina = soup.find_all(
-        'td', class_='views-field views-field-created views-field-title')
+        for item in etiqueta_pagina:
 
-    for item in etiqueta_pagina:
+            fechas_publicacion = item.find(
+                'div', class_='list-datetime bold uppercase font-green-jungle list-datetime-list').get_text(strip=True)
+            list_fechas_convocatorias.append(fechas_publicacion)
 
-        fechas_publicacion = item.find(
-            'div', class_='list-datetime bold uppercase font-green-jungle list-datetime-list').get_text(strip=True)
-        list_fechas_convocatorias.append(fechas_publicacion)
+            titulo = item.find(
+                'div', class_='list-item-content').get_text(strip=True)
+            titulos.append(titulo)
 
-        titulo = item.find(
-            'div', class_='list-item-content').get_text(strip=True)
-        titulos.append(titulo)
+            enlace_subpagina = 'https://www.unileon.es'+item.find('a')['href']
+            enlaces.append(enlace_subpagina)
 
-        enlace_subpagina = 'https://www.unileon.es'+item.find('a')['href']
-        enlaces.append(enlace_subpagina)
+            respuesta_subpagina = requests.get(enlace_subpagina)
 
-        respuesta_subpagina = requests.get(enlace_subpagina)
+            if respuesta_subpagina.status_code == 200:
+                sub_soup = BeautifulSoup(respuesta_subpagina.text, 'lxml')
 
-        if respuesta_subpagina.status_code == 200:
-            sub_soup = BeautifulSoup(respuesta_subpagina.text, 'lxml')
+                if sub_soup.find('div', class_='field field--name-field-announcement-target field--type-list-string field--label-above') == None:
+                    categorias.append('GENERAL')
+                elif 'General' == sub_soup.find('div', class_='field field--name-field-announcement-target field--type-list-string field--label-above').find('div', class_='field--item').get_text(strip=True):
+                    categorias.append('GENERAL')
+                elif 'P.A.S. Laboral' == sub_soup.find('div', class_='field field--name-field-announcement-target field--type-list-string field--label-above').find('div', class_='field--item').get_text(strip=True):
+                    categorias.append('PAS')
+                else:
+                    categorias.append('GENERAL')
 
-            if sub_soup.find('div', class_='field field--name-field-announcement-target field--type-list-string field--label-above') == None:
-                categorias.append('GENERAL')
-            elif 'General' == sub_soup.find('div', class_='field field--name-field-announcement-target field--type-list-string field--label-above').find('div', class_='field--item').get_text(strip=True):
-                categorias.append('GENERAL')
-            elif 'P.A.S. Laboral' == sub_soup.find('div', class_='field field--name-field-announcement-target field--type-list-string field--label-above').find('div', class_='field--item').get_text(strip=True):
-                categorias.append('PAS')
-            else:
-                categorias.append('GENERAL')
+                if sub_soup.find('div', class_='field field--name-field-type field--type-list-string field--label-above') == None:
+                    tipos.append('No especificado')
+                else:
+                    tipo = sub_soup.find('div', class_='field field--name-field-type field--type-list-string field--label-above').find(
+                        'div', class_='field--item').get_text(strip=True)
+                    tipos.append(tipo)
 
-            fecha_inicio_solicitud = sub_soup.find(
-                'div', class_='field field--name-field-announcement-date field--type-datetime field--label-above').find('time')['datetime']
-            list_inicio.append(fecha_inicio_solicitud)
+                fecha_inicio_solicitud = sub_soup.find(
+                    'div', class_='field field--name-field-announcement-date field--type-datetime field--label-above').find('time')['datetime']
+                list_inicio.append(fecha_inicio_solicitud)
 
-            fecha_fin_solicitud = sub_soup.find(
-                'div', class_='field field--name-field-announcement-deadline field--type-datetime field--label-above').find('time')['datetime']
-            list_fin.append(fecha_fin_solicitud)
+                fecha_fin_solicitud = sub_soup.find(
+                    'div', class_='field field--name-field-announcement-deadline field--type-datetime field--label-above').find('time')['datetime']
+                list_fin.append(fecha_fin_solicitud)
 
-            determinar_plazo(fecha_fin_solicitud)
+                determinar_plazo(fecha_fin_solicitud)
 
-            if None == sub_soup.find('div', class_='field field--name-field-positions field--type-string-long field--label-above'):
-                nombre_plazas.append('No especificado')
-            else:
-                nombre_plaza = sub_soup.find('div', class_='field field--name-field-positions field--type-string-long field--label-above').find(
-                    'div', class_='field--item').get_text(strip=True)
-                nombre_plazas.append(nombre_plaza)
+                if None == sub_soup.find('div', class_='field field--name-field-positions field--type-string-long field--label-above'):
+                    nombre_plazas.append('No especificado')
+                else:
+                    nombre_plaza = sub_soup.find('div', class_='field field--name-field-positions field--type-string-long field--label-above').find(
+                        'div', class_='field--item').get_text(strip=True)
+                    nombre_plazas.append(nombre_plaza)
 
+                if sub_soup.find('div', class_='field field--name-field-associated-announcement field--type-string-long field--label-above') == None:
+                    convocatorias_asociadas.append('No especificado')
+                else:
+                    conv_asociada = sub_soup.find('div', class_='field field--name-field-associated-announcement field--type-string-long field--label-above').find(
+                        'div', class_='field--item').get_text(strip=True)
+                    convocatorias_asociadas.append(conv_asociada)
+
+                if sub_soup.find('div', class_='field field--name-body field--type-text-with-summary field--label-hidden field--item') == None:
+                    descripciones.append('No especificado')
+                else:
+                    descripcion = sub_soup.find(
+                        'div', class_='field field--name-body field--type-text-with-summary field--label-hidden field--item').get_text(strip=True)
+                    descripciones.append(descripcion)
+
+                documento = ' '
+                if sub_soup.find('div', class_='field field--name-upload field--type-file field--label-hidden field--items') == None:
+                    documentos_adjuntos.append('No hay documentos adjuntos')
+                else:
+                    lista_documentos = sub_soup.find(
+                        'div', class_='field field--name-upload field--type-file field--label-hidden field--items').find_all('span', class_='file-link')
+
+                    for item in lista_documentos:
+                        documento += item.get_text(strip=True) + ' '
+                    documentos_adjuntos.append(documento)
+
+
+def extraer_datos_paginas():
+    contador_paginas = 0
+    # Link para extraer los datos.
+    link_ule = 'https://www.unileon.es/actualidad/convocatorias'
+    while contador_paginas < 4:
+        if contador_paginas == 0:
+            obtener_datos(link_ule)
+        else:
+            link_ule_siguientes_paginas = 'https://www.unileon.es/actualidad/convocatorias?page=' + \
+                str(contador_paginas)
+            obtener_datos(link_ule_siguientes_paginas)
+            link_ule_siguientes_paginas = ''
+        contador_paginas += 1
+
+
+extraer_datos_paginas()
 
 for i in range(len(titulos)):
     convocantes.append('Universidad de León')
 
-print(titulos)
+'''
 print(list_fechas_convocatorias)
+print(titulos)
 print(enlaces)
 print(categorias)
+print(tipos)
 print(list_inicio)
 print(list_fin)
 print(convocantes)
 print(nombre_plazas)
+print(convocatorias_asociadas)
 print(estados)
 
-
-print(len(titulos))
 print(len(list_fechas_convocatorias))
+print(len(titulos))
 print(len(enlaces))
 print(len(categorias))
+print(len(tipos))
 print(len(list_inicio))
 print(len(list_fin))
 print(len(convocantes))
 print(len(nombre_plazas))
+print(len(convocatorias_asociadas))
 print(len(estados))
 
-print(titulos[0])
+
 print(list_fechas_convocatorias[0])
+print(titulos[0])
 print(enlaces[0])
 print(categorias[0])
+print(tipos[0])
 print(list_inicio[0])
 print(list_fin[0])
 print(convocantes[0])
 print(nombre_plazas[0])
+print(convocatorias_asociadas[0])
 print(estados[0])
+'''
+
+
+for item in titulos:
+    print(item)
+
+print('*************************')
+for item in convocatorias_asociadas:
+    print(item)
+
+print('*************************')
+for item in descripciones:
+    print(item)
+
+print('*************************')
+print(estados)
+
+print('*************************')
+print('*************************')
+print('*************************')
+
+for item in documentos_adjuntos:
+    print(item)
