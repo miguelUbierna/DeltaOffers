2d4b079645438f65269a00c78d307a91b8fc5780
miguelUbierna <m.ubiernagutierrez@gmail.com>
2024-03-19 20:48:39 +0100

    Pequeñas modificaciones scraping UBU

--- Scrapers/ScrapingUBU/scrapingUBU.py
+++ Scrapers/ScrapingUBU/scrapingUBU.py
@@ -4,51 +4,38 @@ from bs4 import BeautifulSoup
 
 class UBUScraper:
     def __init__(self):
-        self.titulos = []
-        self.estados = []
-        self.descripciones = []
-        self.list_fechas_convocatorias = []
-        self.list_inicio = []
-        self.list_fin = []
-        self.convocantes = []
-        self.destinatarios = []
-        self.categorias = []
-        self.enlaces = []
-        self.universidad = []
+        self.filas = []
         self.ubu_general = []
         self.contador_cerradas = 0
 
     def obtener_titulos(self, item):
         titulo = item.find(
             'div', class_='field-content').find('a').get_text(strip=True)
-        self.titulos.append(titulo)
+        self.filas.append(titulo)
 
     def obtener_estados(self, item):
         estado = item.find('div').get_text(strip=True)
-        # En el momento en el que estoy haciendo el scraping, no hay ninguna convocatoria en resolución.
-        # Como no se, cual será el contenido de este tipo de etiquetas, si el estado de la convocatoria,
-        # No es ni en plazo ni cerrado, automaticamente podré como estado: "EN RESOLUCIÓN"
-        if estado != 'EN PLAZO' and estado != 'CONVOCATORIA CERRADA':
-            self.estados.append('EN RESOLUCIÓN')
-        else:
-            self.estados.append(estado)
+        self.filas.append(estado)
 
     def obtener_enlaces_subpagina(self, item):
         # Proceso de obtención de subpágina.
         enlace_subpagina = 'https://www.ubu.es' + item.find('a')['href']
-        self.enlaces.append(enlace_subpagina)
+        self.filas.append(enlace_subpagina)
         respuesta_subpagina = requests.get(enlace_subpagina)
         return respuesta_subpagina
 
     def obtener_descripciones(self, contenedor):
-        descripcion = contenedor.find('div', class_='field-item even', property='content:encoded').p.get_text(
-            strip=True)
-        self.descripciones.append(descripcion)
+        if contenedor.find('div', class_='field-item even', property='content:encoded').p == None:
+            descripcion = 'No especificada'
+        else:
+            descripcion = contenedor.find('div', class_='field-item even', property='content:encoded').p.get_text(
+                strip=True)
+        self.filas.append(descripcion)
 
     def obtener_fechas_convocatorias(self, contenedor):
         fecha_convocatoria = contenedor.find(
             'span', class_='date-display-single')['content']
-        self.list_fechas_convocatorias.append(fecha_convocatoria)
+        self.filas.append(fecha_convocatoria)
 
     def obtener_plazos_solicitud(self, contenedor):
         if str(contenedor.find('div', class_='field field-name-field-presentation-date field-type-datetime '
@@ -56,28 +43,28 @@ class UBUScraper:
 
             fecha_inicio_solicitud = contenedor.find(
                 'span', class_='date-display-start')['content']
-            self.list_inicio.append(fecha_inicio_solicitud)
+            self.filas.append(fecha_inicio_solicitud)
 
             fecha_fin_solicitud = contenedor.find(
                 'span', class_='date-display-end')['content']
-            self.list_fin.append(fecha_fin_solicitud)
+            self.filas.append(fecha_fin_solicitud)
 
         else:
-            self.list_inicio.append('No Especificada')
+            self.filas.append('No Especificada')
             fecha_fin_solicitud = contenedor.find('div', class_='field field-name-field-presentation-date '
                                                   'field-type-datetime field-label-above').find(
                 'span', class_='date-display-single')['content']
-            self.list_fin.append(fecha_fin_solicitud)
+            self.filas.append(fecha_fin_solicitud)
 
     def obtener_convocantes(self, contenedor, convocante):
         if contenedor.find('div', class_='field field-name-field-convener field-type-text-long field-label-above') == None:
-            self.convocantes.append('No especificado')
+            self.filas.append('No especificado')
         else:
             contenedor_convocante = contenedor.find(
                 'div', class_='field field-name-field-convener field-type-text-long field-label-above').find_all('p')
             for item in contenedor_convocante:
                 convocante += item.get_text(strip=True) + ' '
-            self.convocantes.append(convocante)
+            self.filas.append(convocante)
 
     def obtener_destinatarios(self, contenedor):
         if contenedor.find('div', class_='field field-name-field-receiver field-type-text-long field-label-above') \
@@ -85,24 +72,27 @@ class UBUScraper:
             contenedor_destinatarios = contenedor.find(
                 'div', class_='field field-name-field-receiver field-type-text-long field-label-above').find(
                 'div', class_='field-item even').get_text(strip=True)
-            self.destinatarios.append(contenedor_destinatarios)
+            self.filas.append(contenedor_destinatarios)
         else:
-            self.destinatarios.append('No se han encontrado destinatarios')
+            self.filas.append('No se han encontrado destinatarios')
 
     def obtener_categorias(self, contenedor):
         categoria = contenedor.find(
             'div', class_='field-item odd').a.get_text(strip=True)
 
         if categoria == 'Convocatorias personal docente' or categoria == 'Investigación: adscritas a proyectos':
-            self.categorias.append('PDI')
+            self.filas.append('PDI')
         elif categoria == 'Convocatorias PAS':
-            self.categorias.append('PAS')
+            self.filas.append('PAS')
         else:
-            self.categorias.append('No especificada')
+            self.filas.append('No especificada')
 
     def obtener_universidad(self):
-        for _ in range(len(self.titulos)):
-            self.universidad.append('Universidad de Burgos')
+        self.filas.append('Universidad de Burgos')
+
+    def añadir_fila(self):
+        self.ubu_general.append(list(self.filas))
+        self.filas.clear()
 
     def obtener_datos(self, link):
         convocante = ''
@@ -136,7 +126,16 @@ class UBUScraper:
                         self.obtener_convocantes(contenedor, convocante)
                         self.obtener_destinatarios(contenedor)
                         self.obtener_categorias(contenedor)
+                    self.obtener_universidad()
 
+                    if self.filas[1] == 'EN PLAZO' or self.filas[1] == 'EN RESOLUCION':
+                        self.añadir_fila()
+                    elif self.filas[1] == 'CONVOCATORIA CERRADA':
+                        if self.contador_cerradas < 10:
+                            self.añadir_fila()
+                            self.contador_cerradas += 1
+                        else:
+                            self.contador_cerradas += 1
                 else:
                     print(f'La solicitud ha fallado y su código de estado es el {
                         respuesta_subpagina.status_code}')
@@ -145,95 +144,33 @@ class UBUScraper:
             print(f'La solicitud ha fallado y su código de estado es el {
                 response.status_code}')
 
-    def añadir_a_estructura_general(self):
-        self.ubu_general.append(self.titulos)
-        self.ubu_general.append(self.estados)
-        self.ubu_general.append(self.descripciones)
-        self.ubu_general.append(self.list_fechas_convocatorias)
-        self.ubu_general.append(self.list_inicio)
-        self.ubu_general.append(self.list_fin)
-        self.ubu_general.append(self.convocantes)
-        self.ubu_general.append(self.destinatarios)
-        self.ubu_general.append(self.categorias)
-        self.ubu_general.append(self.enlaces)
-        self.ubu_general.append(self.universidad)
-
-        return self.ubu_general
-
     def tabla_limpia(self):
         for fila in self.ubu_general:
             for i, item in enumerate(fila):
                 fila[i] = item.replace('\xa0', ' ')
 
-    def contar_convocatorias_cerradas(self):
-        for item in self.estados:
-            if item == 'CONVOCATORIA CERRADA':
-                self.contador_cerradas += 1
-            else:
-                # Si encuentro una abierta, o en proceso, vuelvo a empezar a contar
-                self.contador_cerradas = 0
-        # Si he recorrido 30 convocatorias y no he encontrado ninguna abierta, suponemos que ya no quedarán mas abiertas.
-        if self.contador_cerradas >= 30:
-            return True
-        else:
-            return False
-
     def extraer_datos_paginas(self):
         contador_paginas = 0
         # Link para extraer los datos.
         link_ubu = 'https://www.ubu.es/trabaja-en-la-ubu'
-        while not self.contar_convocatorias_cerradas():
+        while self.contador_cerradas < 30:
             if contador_paginas == 0:
                 self.obtener_datos(link_ubu)
+                # Meter aqui algo para que rompa el bucle
             else:
                 link_ubu_siguientes_paginas = 'https://www.ubu.es/trabaja-en-la-ubu?field_term_interest_tid=109&page=' + \
                     str(contador_paginas)
-                self.obtener_datos(link_ubu_siguientes_paginas)
+                self.obtener_datos(
+                    link_ubu_siguientes_paginas)
                 link_ubu_siguientes_paginas = ''
+                # Meter aqui algo que rompa el bucle
             contador_paginas += 1
 
 
 scraper = UBUScraper()
 scraper.extraer_datos_paginas()
-scraper.obtener_universidad()
-scraper.añadir_a_estructura_general()
+# Una vez que se rompe el bucle, meter aqui algo para limpiar mi estructura
+print(scraper.ubu_general)
+print(len(scraper.ubu_general))
+print(len(scraper.ubu_general[0]))
 scraper.tabla_limpia()
-
-
-print(scraper.titulos)
-print(scraper.estados)
-print(scraper.descripciones)
-print(scraper.list_fechas_convocatorias)
-print(scraper.list_inicio)
-print(scraper.list_fin)
-print(scraper.convocantes)
-print(scraper.destinatarios)
-print(scraper.categorias)
-print(scraper.enlaces)
-print(scraper.universidad)
-
-
-print(len(scraper.titulos))
-print(len(scraper.estados))
-print(len(scraper.descripciones))
-print(len(scraper.list_fechas_convocatorias))
-print(len(scraper.list_inicio))
-print(len(scraper.list_fin))
-print(len(scraper.convocantes))
-print(len(scraper.destinatarios))
-print(len(scraper.categorias))
-print(len(scraper.enlaces))
-print(len(scraper.universidad))
-
-
-print(scraper.titulos[11])
-print(scraper.estados[11])
-print(scraper.descripciones[11])
-print(scraper.list_fechas_convocatorias[11])
-print(scraper.list_inicio[11])
-print(scraper.list_fin[11])
-print(scraper.convocantes[11])
-print(scraper.destinatarios[11])
-print(scraper.categorias[11])
-print(scraper.enlaces[11])
-print(scraper.universidad[11])
